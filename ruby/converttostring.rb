da652a1a0a3a12a5c098ce3e3ece497f3301c410
Kevin Wong <k.wong4@yahoo.com>
2019-03-01 16:35:56 -0500

    uses to_s to convert num to string

---
+++ ruby/converttostring.rb
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/ruby
+# We need a function that can transform a number into a string.
+
+# What ways of achieving this do you know?
+
+# Examples:
+# number_to_string(123) /* returns '123' */
+# number_to_string(999) /* returns '999' */
+
+def numberToString(num)
+    num.to_s
+end
+
+numberToString(123)
