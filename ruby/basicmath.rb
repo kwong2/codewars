0902b6567bab79854c3a4f7d190425a7574545cb
Kevin Wong <k.wong4@yahoo.com>
2019-03-04 15:40:57 -0500

    solved with case/when

---
+++ ruby/basicmath.rb
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/ruby
+# Your task is to create a function that does four basic mathematical operations.
+
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+
+# Examples:
+
+# basic_op('+', 4, 7)         # Output: 11
+# basic_op('-', 15, 18)       # Output: -3
+# basic_op('*', 5, 5)         # Output: 25
+# basic_op('/', 49, 7)        # Output: 7
+
+def basic_op(operator, value1, value2)
+    #your code here
+    case operator
+    when '+'
+        return (value1 + value2)
+    when '-'
+        return (value1 - value2)
+    when '*'
+        return (value1 * value2)
+    when '/'
+        return (value1 / value2)
+    end
+end
